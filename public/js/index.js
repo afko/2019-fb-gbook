91d6bf1b5712d520843ee435ae385a611457440b
Sungje Kim <sungje.kim@one-line.com>
2019-02-12 09:47:17 +0900

    create framework layout

---
+++ public/js/index.js
@@ -0,0 +1,11 @@
+// from firebase
+var config = {
+    apiKey: "AIzaSyAzq5JO2tcopubdHp6V3widF9qVn8lm1TM",
+    authDomain: "afko-gbook.firebaseapp.com",
+    databaseURL: "https://afko-gbook.firebaseio.com",
+    projectId: "afko-gbook",
+    storageBucket: "afko-gbook.appspot.com",
+    messagingSenderId: "769455268887"
+  };
+  
+  firebase.initializeApp(config);
